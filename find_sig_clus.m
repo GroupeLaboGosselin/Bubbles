9522788c061ac1cc4c05661ec8bd606488d2e544
laurentcaplette <lcaplette2002@msn.com>
2017-12-23 12:31:45 -0500

    Added copyright & license text

--- find_sig_clus.m
+++ find_sig_clus.m
@@ -4,7 +4,20 @@ function ind = find_sig_clus(SCI, smoothDims, clus_type, thresh_clus, clusValThr
 % clusters, and returns the linear indices of the significant indices. See
 % STATCI_MAX function documentation for details.
 %
-% Laurent Caplette, 2017
+% Copyright (c) 2017 Laurent Caplette
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 % Parameters
 nPred = size(SCI);
--- smoothCIs.m
+++ smoothCIs.m
@@ -35,8 +35,22 @@ function [SCI, permSCI] = smoothCIs(CI,permCI,sigma,padding,conds)
 % PERMSCI is an array of smoothed permutation classification images.
 %
 %
+% Copyright (c) 2017, Laurent Caplette & Frédéric Gosselin
 % Code written by Laurent Caplette, 2017
 % Some parts are based on code by Frédéric Gosselin
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 % Defaults
